65a9684853baa2380a084cf6aa7b392fb1ecb9d5
Guillermo <guillermo.hre@gmail.com>
2026-04-20 00:21:44 -0600

    Add DDL scripts for catalog, schemas, tables, and volumes for project

---
+++ project/code/DDL/esquemas.sql
@@ -0,0 +1,4 @@
+-- Crear los esquemas
+CREATE SCHEMA IF NOT EXISTS cor_project.bronze;
+CREATE SCHEMA IF NOT EXISTS cor_project.silver;
+CREATE SCHEMA IF NOT EXISTS cor_project.gold;
---
+++ project/code/DDL/volumenes.sql
@@ -0,0 +1,2 @@
+-- Crear el volumen landing 
+CREATE VOLUME IF NOT EXISTS cor_project.bronze.landing;
