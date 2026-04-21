c9e91e25554ba7cbb9116f28b773671ef3a9468f
Guillermo <guillermo.hre@gmail.com>
2026-04-21 17:43:33 -0600

    Add landing_to_bronze.py for data ingestion with environment variable support

---
+++ code/ELT/landing_to_bronze.py
@@ -0,0 +1,31 @@
+from pyspark.sql.functions import current_timestamp, col
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ambiente")
+args = parser.parse_args()
+
+ambiente = args.ambiente
+print(ambiente)
+
+df = (
+    spark.readStream
+        .format("cloudFiles")
+        .option("cloudFiles.format", "text")
+        .option("cloudFiles.useIncrementalListing", "true")
+        .load(f"/Volumes/cor_{ambiente}/bronze/landing/data/")
+)
+
+df_transformed = df.select(
+    col("value").alias("data"),
+    col("_metadata.file_path").alias("source_file"),
+    current_timestamp().alias("ingestion_timestamp")
+)
+
+(
+    df_transformed.writeStream
+        .format("delta")
+        .trigger(availableNow=True)
+        .option("checkpointLocation", f"/Volumes/cor_{ambiente}/bronze/landing/checkpoints/raw_swell_metrics")
+        .toTable(f"cor_{ambiente}.bronze.raw_swell_metrics")
+)
