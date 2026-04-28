285b39413106f8d60e7efcbd6879e6de4c8a6eaa
guillermo.hre@gmail.com <guillermo.hre@gmail.com>
2026-04-28 16:32:16 +0000

    agregar estructura para comentarios de tablas y columnas

--- code/DDL/create_tables.py
+++ code/DDL/create_tables.py
@@ -11,10 +11,14 @@ print(ambiente)
 spark.sql(
     f"""
         CREATE TABLE IF NOT EXISTS cor_{ambiente}.bronze.raw_swell_metrics (
-            data STRING,
-            source_file STRING,
+            data STRING
+                COMMENT '',
+            source_file STRING
+                COMMENT '',
             ingestion_timestamp TIMESTAMP
+                COMMENT ''
         ) USING DELTA
+            COMMENT ''
     """
 )
 
@@ -22,37 +26,61 @@ spark.sql(
 spark.sql(
     f"""
         CREATE TABLE IF NOT EXISTS cor_{ambiente}.silver.swell_metrics (
-            coast_name STRING,
-            datetime TIMESTAMP,
-            year INTEGER,
-            wind_speed_ms FLOAT,
-            wind_direction_deg FLOAT,
-            wind_u FLOAT,
-            wind_v FLOAT,
-            wave_height_m FLOAT,
-            wave_direction_deg FLOAT,
-            wave_u FLOAT,
-            wave_v FLOAT,
-            wave_period_s FLOAT,
-            ingestion_timestamp TIMESTAMP,
-            source_file STRING,
+            id BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1)
+                COMMENT '',
+            coast_name STRING
+                COMMENT '',
+            datetime TIMESTAMP
+                COMMENT '',
+            year INTEGER
+                COMMENT '',
+            wind_speed_ms FLOAT
+                COMMENT '',
+            wind_direction_deg FLOAT
+                COMMENT '',
+            wind_u FLOAT
+                COMMENT '',
+            wind_v FLOAT
+                COMMENT '',
+            wave_height_m FLOAT
+                COMMENT '',
+            wave_direction_deg FLOAT
+                COMMENT '',
+            wave_u FLOAT
+                COMMENT '',
+            wave_v FLOAT
+                COMMENT '',
+            wave_period_s FLOAT
+                COMMENT '',
+            ingestion_timestamp TIMESTAMP
+                COMMENT '',
+            source_file STRING
+                COMMENT '',
             transformation_timestamp TIMESTAMP
+                COMMENT ''
         )
         USING DELTA
         PARTITIONED BY (coast_name, year)
+            COMMENT ''
     """
 )
 
 spark.sql(
     f"""
         CREATE TABLE IF NOT EXISTS cor_{ambiente}.silver.quarantine_swell_metrics (
-            data STRING,
-            source_file STRING,
-            error_reason STRING,
-            ingestion_timestamp TIMESTAMP,
+            data STRING
+                COMMENT '',
+            source_file STRING
+                COMMENT '',
+            error_reason STRING
+                COMMENT '',
+            ingestion_timestamp TIMESTAMP
+                COMMENT '',
             transformation_timestamp TIMESTAMP
+                COMMENT ''
         )
         USING DELTA
+            COMMENT ''
     """
 )
 
