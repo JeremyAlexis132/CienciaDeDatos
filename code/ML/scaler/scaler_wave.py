4f8c08bc7c03a68a52b21129b7ba85a67df986fb
Guillermo <guillermo.hre@gmail.com>
2026-05-03 09:57:19 -0600

    crear logica para almacenar en volumes o local y crear un scaler por cpsta

--- code/ML/scaler/scaler_wave.py
+++ code/ML/scaler/scaler_wave.py
@@ -1,5 +1,7 @@
 from sklearn.preprocessing import StandardScaler
 import joblib
+import os
+from pathlib import Path
 from pyspark.sql import functions as F
 import argparse
 
@@ -9,7 +11,7 @@ args = parser.parse_args()
 
 ambiente = args.ambiente
 print(ambiente)
-PORCENTAJE_ENTRENAMIENTO = 0.2
+PORCENTAJE_ENTRENAMIENTO = 0.5
 
 data = (
     spark.sql(
@@ -20,22 +22,32 @@ data = (
     )
 )
 
-data_pre_processing= (
+data_pre_processing = (
     data
     .withColumn('coast_year_month', F.concat(F.col('coast_name'), F.lit('_'), F.date_format('datetime', 'yyyy-MM')))
 )
 
 coast_year_month_dict = {row.coast_year_month: PORCENTAJE_ENTRENAMIENTO for row in data_pre_processing.select('coast_year_month').distinct().collect()}
+
 data_sample = (
     data_pre_processing
     .sampleBy('coast_year_month', fractions=coast_year_month_dict, seed=0)
     .drop('coast_year_month')
 ).toPandas()
 
+coast_names = data_sample['coast_name'].unique()
+
 features = ['wind_u', 'wind_v', 'wave_u', 'wave_v', 'wave_period_s']
 
 scaler = StandardScaler()
-X = scaler.fit_transform(data_sample[features])
 
-path = f'/Volumes/cor_{ambiente}/ml/models/scaler/scaler.pkl'
-joblib.dump(scaler, path)
+if 'DATABRICKS_RUNTIME_VERSION' in os.environ:
+    scaler_path = f'/Volumes/cor_{ambiente}/ml/models/scaler/scaler_{{}}.pkl'
+else:
+    base_path = Path.cwd().parent
+    scaler_path = f'{base_path}/scaler/scaler_{{}}.pkl'
+
+for coast in coast_names:
+    X = data_sample[data_sample['coast_name'] == coast][features]
+    scaler.fit(X)
+    joblib.dump(scaler, scaler_path.format(coast))
