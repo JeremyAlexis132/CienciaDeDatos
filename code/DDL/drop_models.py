5d35fab1d289d5b49b1254f2f124acc2c2e0f0e6
Guillermo <guillermo.hre@gmail.com>
2026-05-28 20:31:45 -0600

    feat: add drop_models task to cor_reset_tables.yml and implement drop_models.py script

---
+++ code/DDL/drop_models.py
@@ -0,0 +1,10 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ambiente")
+args = parser.parse_args()
+
+ambiente = args.ambiente
+print(ambiente)
+
+spark.sql(f"DROP VOLUME IF EXISTS cor_{ambiente}.ml.models")
